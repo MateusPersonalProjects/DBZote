ba226db606657541d6f21a7110c96c04491122b1
Mateus Oliveira <mateusaugusto.godoy@gmail.com>
2024-02-26 08:43:51 -0300

    Implementing the pager

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 /*
  Define the InputBuffer for the REPL
@@ -69,10 +72,17 @@ const uint32_t PAGE_SIZE = 4096;
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
-/* Define our table */
+/* Define the Pager */
+typedef struct {
+  int file_descriptor;
+  uint32_t file_lenght;
+  void* pages[TABLE_MAX_PAGES];
+} Pager;
+
+/* Define the Table */
 typedef struct {
   uint32_t num_rows;
-  void* pages[TABLE_MAX_PAGES];
+  Pager* pager;
 } Table;
 
 /* Prints a row of the table */
@@ -94,12 +104,40 @@ void deserialize_row(void* source, Row* destination) {
   memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
+void* get_page(Pager* pager, uint32_t page_num) {
+  if (page_num > TABLE_MAX_PAGES) {
+    printf("Tried to fetch page number out of bounds. %d > %d\n", page_num,
+           TABLE_MAX_PAGES);
+    exit(EXIT_FAILURE);
+  }
+
+  if (pager->pages[page_num] == NULL) {
+    // If it is NULL we need to locate memory for the page
+    void* page = malloc(PAGE_SIZE);
+    uint32_t num_pages = pager->file_lenght / PAGE_SIZE;
+
+    // Problem with partial page (With B tree we are going to solve this)
+    if (pager->file_lenght % PAGE_SIZE) num_pages += 1;
+
+    if (page_num <= num_pages) {
+      lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+      ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+      if (bytes_read == -1) {
+        printf("Error reading file: %d\n", errno);
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    pager->pages[page_num] = page;
+  }
+
+  return pager->pages[page_num];
+}
+
 /* Getting the next row inside a page to use */
 void* row_slot(Table* table, uint32_t row_num) {
   uint32_t page_num = row_num / ROWS_PER_PAGE;
-  void* page = table->pages[page_num];
-  // Memory is allocated when we try to access the page
-  if (page == NULL) page = table->pages[page_num] = malloc(PAGE_SIZE);
+  void* page = get_page(table->pager, page_num);
   uint32_t row_offset = row_num % ROWS_PER_PAGE;
   uint32_t byte_offset = row_offset * ROW_SIZE;
 
@@ -108,8 +146,7 @@ void* row_slot(Table* table, uint32_t row_num) {
 
 /* Creates a new table */
 Table* new_table() {
-  Table* table = (Table*)malloc(sizeof(Table));
-  table->num_rows = 0;
+  /* ----------- PAREI AQUI ---------------*/
   for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) table->pages[i] = NULL;
 
   return table;
@@ -199,7 +236,7 @@ PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement) {
   if (strlen(username) > COLUMN_USERNAME_SIZE) return PREPARE_STRING_TOO_LONG;
   if (strlen(email) > COLUMN_EMAIL_SIZE) return PREPARE_STRING_TOO_LONG;
 
-  // If everything is allright let get it to store o/
+  // If everything is all right lets get it to store o/
   statement->row_to_insert.id = id;
   strcpy(statement->row_to_insert.username, username);
   strcpy(statement->row_to_insert.email, email);
@@ -269,6 +306,7 @@ int main(int argc, char* argv[]) {
     print_prompt();
     read_input(input_buffer);
 
+    // If user inputed a meta command
     if (input_buffer->buffer[0] == '.') {
       switch (do_meta_command(input_buffer, table)) {
         case (META_COMMAND_SUCCESS):
